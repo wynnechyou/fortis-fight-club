68a86f637645f677c9bb28335bae0add2f2cd0c7
wynnechyou <wynne.chyou@fortisgames.com>
2026-04-23 16:13:57 -0700

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   root: "app",
+  base: "/fortis-fight-club/",
   plugins: [react()],
   publicDir: "../data",
   resolve: {
